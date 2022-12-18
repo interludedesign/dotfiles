71e29b4c44ceb44eba5eb7d8fa3cfe66a2caa486
Chris Swann <chris@interludedesign.com>
2022-12-18 15:07:16 -0800

    Platform independence

--- .fzf.zsh
+++ .fzf.zsh
@@ -6,8 +6,8 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
