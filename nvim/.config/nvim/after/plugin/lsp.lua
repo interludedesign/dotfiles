101ef43e7380e77cd9104ef42342da2d2966f1ce
Chris Swann <chris@interludedesign.com>
2024-04-12 21:54:30 -0700

    Add keymap for LSP code actions

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -53,6 +53,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
 end)
 
 lsp.setup()
