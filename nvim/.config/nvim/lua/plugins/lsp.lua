22d368947f01c71b4b21669ad8bbe9fd19da018e
Chris Swann <chris@interludedesign.com>
2025-01-20 16:54:40 -0800

    Add dap and starship

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,56 +1,68 @@
 return {
-    {
-        "neovim/nvim-lspconfig", -- Core LSP support
-        dependencies = {
-            "williamboman/mason.nvim", -- Mason for managing LSP servers
-            "williamboman/mason-lspconfig.nvim", -- Mason bridge for nvim-lspconfig
-            "hrsh7th/nvim-cmp", -- Autocompletion plugin
-            "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+  {
+    "neovim/nvim-lspconfig", -- Core LSP support
+    dependencies = {
+      "williamboman/mason.nvim", -- Mason for managing LSP servers
+      "williamboman/mason-lspconfig.nvim", -- Mason bridge for nvim-lspconfig
+      "hrsh7th/nvim-cmp", -- Autocompletion plugin
+      "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
-        config = function()
-            local mason = require("mason")
-            local mason_lspconfig = require("mason-lspconfig")
-            local lspconfig = require("lspconfig")
-            local cmp = require("cmp")
-            local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-            -- Initialize Mason
-            mason.setup()
-
-            -- Basic `on_attach` function for LSP keybindings
-            local on_attach = function(_, bufnr)
-                local opts = { buffer = bufnr }
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-            end
-
-            -- Enable capabilities for `nvim-cmp`
-            local capabilities = cmp_nvim_lsp.default_capabilities()
-
-            -- Automatically set up servers installed by Mason
-            mason_lspconfig.setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-            })
-
-            -- Set up `nvim-cmp` for autocompletion
-            cmp.setup({
-                mapping = {
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                },
-                sources = {
-                    { name = "nvim_lsp" },
-                },
-            })
-        end,
+      },
     },
+    config = function()
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      local cmp = require("cmp")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+      -- Initialize Mason
+      mason.setup()
+
+      -- Basic `on_attach` function for LSP keybindings
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end
+
+      -- Enable capabilities for `nvim-cmp`
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+
+      -- Automatically set up servers installed by Mason
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+      })
+
+      -- Set up `nvim-cmp` for autocompletion
+      cmp.setup({
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
+    end,
+  },
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,14 +1,4 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-source "$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme"
 source $HOME/.zsh_profile
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 export NVM_DIR="$HOME/.nvm"
 if [ -s "$NVM_DIR/nvm.sh" ]; then
@@ -21,6 +11,8 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+eval "$(starship init zsh)"
 
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
