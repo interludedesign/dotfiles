6ddac229b203f79fb0a8a7b415e1b6516f145b08
Chris Swann <chris@interludedesign.com>
2025-07-30 07:56:22 -0700

    Use latest lsp API

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,78 +1,64 @@
 return {
   {
-    "neovim/nvim-lspconfig", -- Core LSP support
-    dependencies = {
-      "williamboman/mason.nvim", -- Mason for managing LSP servers
-      "williamboman/mason-lspconfig.nvim", -- Mason bridge for nvim-lspconfig
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-    },
+    "williamboman/mason.nvim", -- Mason for managing LSP servers  
     config = function()
-      local mason = require("mason")
-      local mason_lspconfig = require("mason-lspconfig")
-      local lspconfig = require("lspconfig")
-
-      -- Initialize Mason
-      mason.setup()
-
-      -- Basic `on_attach` function for LSP keybindings
-      local on_attach = function(_, bufnr)
-        local opts = { buffer = bufnr }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      end
-
-      -- Enhanced capabilities with blink.cmp
+      require("mason").setup()
+    end,
+  },
+  {
+    "saghen/blink.cmp", -- Completion engine
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      -- Setup global LSP capabilities with blink.cmp
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      -- Configure LSP floating windows with borders and padding
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = "rounded",
-      })
-      
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-        border = "rounded",
+      -- Configure global LSP settings using the new vim.lsp.config API
+      vim.lsp.config('*', {
+        capabilities = capabilities,
       })
 
-      -- Automatically set up servers installed by Mason
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        -- Ruby LSP with YARD support
-        ["ruby_lsp"] = function()
-          lspconfig.ruby_lsp.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-            init_options = {
-              enabledFeatures = {
-                documentSymbols = true,
-                foldingRanges = true,
-                selectionRanges = true,
-                semanticHighlighting = true,
-                formatting = true,
-                codeActions = true,
-              },
-            },
-          })
-        end,
+      -- Ruby LSP specific configuration
+      vim.lsp.config('ruby_lsp', {
+        capabilities = capabilities,
+        init_options = {
+          enabledFeatures = {
+            documentSymbols = true,
+            foldingRanges = true,
+            selectionRanges = true,
+            semanticHighlighting = true,
+            formatting = true,
+            codeActions = true,
+          },
+        },
       })
 
+      -- Enable common LSP servers directly
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('ruby_lsp')
+      vim.lsp.enable('ts_ls') -- TypeScript
+
+      -- LSP keybindings - set globally since handlers changed in 0.11
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local bufnr = args.buf
+          local opts = { buffer = bufnr }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
+      })
     end,
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
   },
 }
