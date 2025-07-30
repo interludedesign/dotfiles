a83df66cb915a2593afe732773819c6b0859b57b
Chris Swann <chris@interludedesign.com>
2025-07-30 07:56:37 -0700

    Try codecompanion

---
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,37 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+    "nvim-telescope/telescope.nvim", -- Optional: For using the telescope prompt
+    {
+      "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+      opts = {},
+    },
+  },
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "anthropic",
+        },
+        agent = {
+          adapter = "anthropic",
+        },
+      },
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "ANTHROPIC_API_KEY",
+            },
+          })
+        end,
+      },
+    })
+  end,
+}
