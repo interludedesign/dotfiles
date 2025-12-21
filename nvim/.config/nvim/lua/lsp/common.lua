ab71289d91b1680c86d5d9617c13bcf0ba139bff
Chris Swann <cswann@hey.com>
2025-12-21 15:44:30 -0800

    Add keymaps for viewing lsp errors

--- nvim/.config/nvim/lua/lsp/common.lua
+++ nvim/.config/nvim/lua/lsp/common.lua
@@ -10,6 +10,11 @@ function M.on_attach(client, bufnr)
   map("n", "gi", vim.lsp.buf.implementation)
   map("n", "gr", vim.lsp.buf.references)
   map("n", "<leader>ca", vim.lsp.buf.code_action)
+  map("n", "gl", vim.diagnostic.open_float)
+  map("n", "ql", function()
+    vim.diagnostic.setqflist()
+    vim.cmd("copen")
+  end)
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
