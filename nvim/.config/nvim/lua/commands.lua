82d07455a4abea67466ccca585724b6e61c44086
Chris Swann <chris@interludedesign.com>
2026-03-19 17:47:24 -0700

    Add test command

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -1,6 +1,7 @@
 local command = vim.api.nvim_create_user_command
 local docs_util = require("utils.docs")
 local date_util = require("utils.date")
+local testing_util = require("utils.testing")
 
 command("DisableDiagnostics", ":lua vim.diagnostic.disable()", {})
 command("LspInfo", ":checkhealth vim.lsp", {})
@@ -9,3 +10,7 @@ command("ExtractSection", function()
 end, { range = true })
 
 date_util.setup()
+
+command("Test", function()
+  testing_util.dotnet_test_to_clipboard()
+end, { desc = "Copy dotnet test command for current file to clipboard" })
--- nvim/.config/nvim/lua/utils/testing.lua
+++ nvim/.config/nvim/lua/utils/testing.lua
@@ -25,4 +25,42 @@ function testing.run_spec_in_tmux()
   require("harpoon.tmux").sendCommand("%0", cmd)
 end
 
+function testing.dotnet_test_to_clipboard()
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+  local namespace = nil
+  local classname = nil
+
+  for _, line in ipairs(lines) do
+    if not namespace then
+      -- matches both block-scoped and file-scoped namespaces
+      local ns = line:match("^%s*namespace%s+([%w%.]+)")
+      if ns then
+        namespace = ns
+      end
+    end
+    if not classname then
+      local cls = line:match("[^%w]class%s+([%w_]+)")
+        or line:match("^class%s+([%w_]+)")
+      if cls then
+        classname = cls
+      end
+    end
+    if namespace and classname then
+      break
+    end
+  end
+
+  if not classname then
+    vim.notify("Could not determine class name from current file", vim.log.levels.WARN)
+    return
+  end
+
+  local filter = namespace and (namespace .. "." .. classname) or classname
+  local cmd = string.format('dotnet test --filter "FullyQualifiedName~%s"', filter)
+
+  vim.fn.setreg("+", cmd)
+  vim.notify("Copied: " .. cmd, vim.log.levels.INFO)
+end
+
 return testing
