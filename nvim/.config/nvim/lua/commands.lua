861d38c3962c1d7900e2374ca0d98a907f702eb6
Chris Swann <chris@interludedesign.com>
2026-03-20 18:20:20 -0700

    case converters

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -2,6 +2,7 @@ local command = vim.api.nvim_create_user_command
 local docs_util = require("utils.docs")
 local date_util = require("utils.date")
 local testing_util = require("utils.testing")
+local case_converters = require("utils.case_converters")
 
 command("DisableDiagnostics", ":lua vim.diagnostic.disable()", {})
 command("LspInfo", ":checkhealth vim.lsp", {})
@@ -14,3 +15,11 @@ date_util.setup()
 command("Test", function()
   testing_util.dotnet_test_to_clipboard()
 end, { desc = "Copy dotnet test command for current file to clipboard" })
+
+command("Pascal", function()
+  case_converters.to_pascal()
+end, { desc = "Convert word under cursor to PascalCase (auto-detects snake, camel, kebab, etc.)" })
+
+command("Camel", function()
+  case_converters.to_camel()
+end, { desc = "Convert word under cursor to camelCase (auto-detects snake, pascal, kebab, etc.)" })
--- nvim/.config/nvim/lua/utils/case_converters.lua
+++ nvim/.config/nvim/lua/utils/case_converters.lua
@@ -1,18 +1,90 @@
 local case_converters = {}
 
-function case_converters.snake_to_pascal()
+local function detect_case(word)
+  if word:find("_") then
+    if word == word:upper() then
+      return "screaming_snake"
+    end
+    return "snake"
+  elseif word:find("-") then
+    return "kebab"
+  elseif word:match("^%l") and word:find("%u") then
+    return "camel"
+  elseif word:match("^%u") and word:find("%u", 2) and not word:find("[_%-]") then
+    return "pascal"
+  elseif word == word:upper() then
+    return "upper"
+  else
+    return "unknown"
+  end
+end
+
+local function split_into_words(word, case)
+  local words = {}
+  if case == "snake" or case == "screaming_snake" then
+    for part in word:lower():gmatch("[^_]+") do
+      table.insert(words, part)
+    end
+  elseif case == "kebab" then
+    for part in word:lower():gmatch("[^%-]+") do
+      table.insert(words, part)
+    end
+  elseif case == "camel" or case == "pascal" then
+    -- Split on uppercase boundaries
+    local result = word:gsub("(%u+)(%u%l)", "%1_%2"):gsub("(%l)(%u)", "%1_%2")
+    for part in result:lower():gmatch("[^_]+") do
+      table.insert(words, part)
+    end
+  else
+    table.insert(words, word:lower())
+  end
+  return words
+end
+
+local function words_to_pascal(words)
+  local result = ""
+  for _, w in ipairs(words) do
+    result = result .. w:sub(1, 1):upper() .. w:sub(2)
+  end
+  return result
+end
+
+local function words_to_camel(words)
+  local result = ""
+  for i, w in ipairs(words) do
+    if i == 1 then
+      result = w:lower()
+    else
+      result = result .. w:sub(1, 1):upper() .. w:sub(2)
+    end
+  end
+  return result
+end
+
+function case_converters.to_camel()
   local word = vim.fn.expand('<cword>')
+  local case = detect_case(word)
+  local words = split_into_words(word, case)
+  local camel = words_to_camel(words)
+  vim.cmd('normal! ciw' .. camel)
+end
 
-  -- Convert to PascalCase
-  local pascal = word:gsub("_(%l)", function(c)
-    return c:upper()
-  end):gsub("^%l", string.upper)
-
-  -- Replace the word under the cursor
+function case_converters.to_pascal()
+  local word = vim.fn.expand('<cword>')
+  local case = detect_case(word)
+  local words = split_into_words(word, case)
+  local pascal = words_to_pascal(words)
   vim.cmd('normal! ciw' .. pascal)
 end
 
--- Map it to <leader>tc
+function case_converters.snake_to_pascal()
+  local word = vim.fn.expand('<cword>')
+  local pascal = word:gsub("_(%l)", function(c)
+    return c:upper()
+  end):gsub("^%l", string.upper)
+  vim.cmd('normal! ciw' .. pascal)
+end
+
 vim.keymap.set('n', '<leader>tc', case_converters.snake_to_pascal, { desc = "Convert snake_case to PascalCase" })
 
 return case_converters
